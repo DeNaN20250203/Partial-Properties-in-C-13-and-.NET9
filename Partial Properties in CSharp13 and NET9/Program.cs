fc861058ec2fd4650ae698c79a00ba494ce0f3f2
ДеЮра <doomov@yandex.ru>
2025-02-11 14:22:34 +0700

    Update Program.cs

--- Partial Properties in CSharp13 and NET9/Program.cs
+++ Partial Properties in CSharp13 and NET9/Program.cs
@@ -1,17 +1,17 @@
 ﻿namespace Partial_Properties_in_CSharp13_and_NET9
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+	internal class Program
+	{
+        	static void Main(string[] args)
+		{
 			Console.WriteLine("Hello, .NET9 and C#13");
 			Customer customer = new Customer();
 			customer.Name = "John Doe";
 			Console.WriteLine(customer.Name);
 
 			Console.ReadKey();
-        }
-    }
+        	}
+	}
 
 	public partial class Customer
 	{
