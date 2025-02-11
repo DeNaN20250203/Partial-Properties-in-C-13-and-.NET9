8c1ed5a083fedb12d8d8b971d0cd39caa6698971
ДеЮра <doomov@yandex.ru>
2025-02-11 14:20:44 +0700

    Add files via upload

---
+++ Partial Properties in CSharp12 and NET8/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Partial_Properties_in_CSharp12_and_NET8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, .NET8 and C#12");
+			Customer customer = new Customer();
+			customer.SetName("John Doe");
+			Console.WriteLine(customer.GetName());
+
+			Console.ReadKey();
+		}
+	}
+	public partial class Customer
+	{
+		private string? name;
+
+		public string? GetName()
+		{
+			return name;
+		}
+
+		public void SetName(string name)
+		{
+			this.name = name;
+		}
+	}
+}
